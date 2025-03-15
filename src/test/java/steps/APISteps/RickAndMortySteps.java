e3fec91a7e1990a1cfc1e83ba2d0c4404dce64da
tinobboy <luksbreaking@gmail.com>
2025-03-15 19:21:57 -0300

    * Agrego escenario nuevo

--- src/test/java/steps/APISteps/RickAndMortySteps.java
+++ src/test/java/steps/APISteps/RickAndMortySteps.java
@@ -8,20 +8,24 @@ import io.cucumber.java.Before;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import io.cucumber.java.sl.In;
 import io.qameta.allure.Allure;
 import io.restassured.http.ContentType;
+import org.junit.Assume;
 import steps.BaseSteps;
 import steps.RequestSteps;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class RickAndMortySteps {
 
     private static BaseSteps step = new BaseSteps();
-    private ResponseSpecification respuestaActual;
+    private ResponseSpecification respuestaActual,respuestaActualCaracteres;
     private RequestSpecification requestSpecification;
     private int id;
+    List<HashMap<String, Object>> personajes = new ArrayList<>();
 
     @Before("@test")
     public void beforeScenario() {
@@ -56,9 +60,15 @@ public class RickAndMortySteps {
 
     }
 
-    @Dado("^la api el episodio 1$")
+    @Dado("^la api del episodio 1$")
     public void laApiElEpisodio1() {
         requestSpecification.setPaths("/episode/1");
+    }
+
+    @Dado("^la ejecucion de la api de personajes y comparando contra la del episodio (.*)$")
+    public void laEjecucionDeLaApiDePersonajesYComparandoContraLaDelEpisodioNroEpisodio(String nroEpisodio) {
+        personajes = ejecutarApiCaracteres();
+        requestSpecification.setPaths("/episode/"+nroEpisodio+"");
     }
 
     @Cuando("^se ejecuta el request$")
@@ -74,11 +84,12 @@ public class RickAndMortySteps {
 
     @Entonces("^se obtiene los personajes que aparecieron en el episodio 1 y se imprime el nombre$")
     public void seObtieneLosPersonajesQueAparecieronEnElEpisodio1YSeImprimeElNombre() {
-        int cantidadDeEpisodios = respuestaActual.getBody().get("characters").size();
+        step.validateResponseCodeyContentType(respuestaActual, 200, ContentType.JSON);
+        int cantidadDeCaracteres = respuestaActual.getBody().get("characters").size();
         List<Integer> idPersonaje = new ArrayList<>();
         List<String> nombresDePersonajes = new ArrayList<>();
 
-        for (int i = 0; i < cantidadDeEpisodios; i++) {
+        for (int i = 0; i < cantidadDeCaracteres; i++) {
             String urlPersonaje = String.valueOf(respuestaActual.getBody().get("characters").get(i));
             int ultimoNumero = extraerUltimoNumero(urlPersonaje);
             idPersonaje.add(ultimoNumero);
@@ -102,6 +113,58 @@ public class RickAndMortySteps {
         Allure.addAttachment("Los Personajes que se encuentran en el episodio 1 son :", String.valueOf(nombresDePersonajes));
         System.out.println(nombresDePersonajes);
     }
+
+    @Entonces("se obtiene los nombres de personajes que aparecieron")
+    public void seObtieneLosNombresDePersonajesQueAparecieron() {
+        step.validateResponseCodeyContentType(respuestaActual, 200, ContentType.JSON);
+        int cantidadDeCaracteres = respuestaActual.getBody().get("characters").size();
+        List<Integer> idPersonajesEpisodio = new ArrayList<>();
+        List<String> nombresDePersonajes = new ArrayList<>();
+
+        for (int i = 0; i < cantidadDeCaracteres; i++) {
+            String urlPersonaje = String.valueOf(respuestaActual.getBody().get("characters").get(i));
+            int ultimoNumero = extraerUltimoNumero(urlPersonaje);
+            idPersonajesEpisodio.add(ultimoNumero);
+        }
+
+        for (HashMap<String, Object> personaje : personajes) {
+            int idPersonaje = Integer.parseInt(String.valueOf(personaje.get("id")));
+            if (idPersonajesEpisodio.contains(idPersonaje)) {
+                nombresDePersonajes.add(String.valueOf(personaje.get("name")));
+            }
+        }
+        System.out.println("Personajes que aparecieron en el episodio:");
+        for (String nombre : nombresDePersonajes) {
+            System.out.println(nombre);
+            Allure.addAttachment("Los Personajes que se encuentran en el episodio 1 son :", String.valueOf(nombre));
+        }
+
+        Assume.assumeFalse("No se encontraron personajes en el episodio",nombresDePersonajes.isEmpty());
+    }
+
+    /**
+     * Metodo que ejecuta api de los caracteres y devuelve una lista de hashmap string object
+     */
+    public List<HashMap<String, Object>> ejecutarApiCaracteres(){
+
+        requestSpecification.setPaths("/character");
+        respuestaActualCaracteres = new RequestSteps().executeRequest(requestSpecification);
+        step.validateResponseCodeyContentType(respuestaActualCaracteres, 200, ContentType.JSON);
+
+        int cantidadDeCaracteres = respuestaActualCaracteres.getBody().get("results").size();
+
+        for (int i = 0; i < cantidadDeCaracteres; i++) {
+            int idPersonaje = Integer.parseInt(respuestaActualCaracteres.getBody().get("results").get(i).get("id").asText());
+            String nombrePersonaje = String.valueOf(respuestaActualCaracteres.getBody().get("results").get(i).get("name"));
+            HashMap<String, Object> personaje = new HashMap<>();
+            personaje.put("id", idPersonaje);
+            personaje.put("name", nombrePersonaje);
+
+            personajes.add(personaje);
+        }
+        return personajes;
+    }
+
 
     /**
      * Método para extraer el último número de una URL
--- src/test/resources/features/RickAndMorty.feature
+++ src/test/resources/features/RickAndMorty.feature
@@ -8,15 +8,27 @@ Característica: Rick and Morty Test
     Entonces el resultado fue exitoso
 
     Ejemplos:
-    |personaje   |
-    |Rick Sanchez|
-    |Morty Smith |
-    |Summer Smith|
-    |Beth Smith  |
-    |Jerry Smith |
+      | personaje    |
+      | Rick Sanchez |
+      | Morty Smith  |
+      | Summer Smith |
+      | Beth Smith   |
+      | Jerry Smith  |
 
-    @test
-    Escenario: Se obtiene los personajes segun el episodio
-      Dado la api el episodio 1
-      Cuando se ejecuta el request
-      Entonces se obtiene los personajes que aparecieron en el episodio 1 y se imprime el nombre
+  @test
+  Escenario: Se obtiene los personajes del episodio 1
+    Dado la api del episodio 1
+    Cuando se ejecuta el request
+    Entonces se obtiene los personajes que aparecieron en el episodio 1 y se imprime el nombre
+
+  @test
+  Esquema del escenario: Se imprime el nombre de cada personaje para el episodio <nroEpisodio>
+    Dado la ejecucion de la api de personajes y comparando contra la del episodio <nroEpisodio>
+    Cuando se ejecuta el request
+    Entonces se obtiene los nombres de personajes que aparecieron
+    Ejemplos:
+    |nroEpisodio|
+    | 1         |
+    | 3         |
+    | 7         |
+
